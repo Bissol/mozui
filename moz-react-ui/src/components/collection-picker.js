0091cfab0a63c4c54c13fe959aec335f5dd2acbb
Bissol <stephane.bissol@gmail.com>
2017-01-22 23:00:49 +0100

    collection picker

---
+++ moz-react-ui/src/components/collection-picker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CollectionPicker extends Component {
+  render() {
+    return (
+      <div className="CollectionPicker">
+        <h3 className="">Collections à utiliser</h3>
+      </div>
+    );
+  }
+}
+
+export default CollectionPicker;
